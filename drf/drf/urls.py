568c80d8f0bacc8673c42ddbd31a3d22e7c4149e
Noe Cabanillas <73083315+nCabanillas@users.noreply.github.com>
2023-10-16 10:22:43 -0500

    added rest_framework setting for documentation

--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -21,5 +21,5 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include('api.urls')),
-    path("docs/", include_docs_urls(title='Api documentation#'))
+    path("docs/", include_docs_urls(title='Api documentation'))
 ]
